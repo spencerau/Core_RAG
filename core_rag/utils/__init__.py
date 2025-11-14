d0594b62878b7fd5c67a6b3e9f693d3930c7805a
Spencer Au <spencerau96@gmail.com>
2025-11-13 20:31:36 -0800

    update utils/__init__.py

--- core_rag/utils/__init__.py
+++ core_rag/utils/__init__.py
@@ -1,5 +1,5 @@
 from .config_loader import load_config
 from .ollama_api import OllamaAPI
-from .text_preprocessing import preprocess_text
+from .text_preprocessing import preprocess_for_embedding
 
-__all__ = ['load_config', 'OllamaAPI', 'preprocess_text']
+__all__ = ['load_config', 'OllamaAPI', 'preprocess_for_embedding']
